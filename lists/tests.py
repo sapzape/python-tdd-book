a71d5477c7c2c2c03d638b2b0ecfa68da52a438b
PeterKim <31546782+sapzape@users.noreply.github.com>
2018-03-26 23:07:32 +0900

    Passing Python Variables to Be Rendered in the Template

--- lists/tests.py
+++ lists/tests.py
@@ -35,3 +35,24 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+    
+    def test_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = '신규 작업 아이템'
+
+        response = home_page(request)
+
+        self.assertIn('신규 작업 아이템', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': '신규 작업 아이템'}
+        )
+        self.assertEqual(response.content.decode(), expected_html)
+
+        ## 신규 예제 코드 - Processing a POST Request on the Server
+        ## (https://www.obeythetestinggoat.com/book/chapter_post
+        ## _and_database.html)
+        # response = self.client.post('/', data={'item_text': 'A new list item'})
+        # self.assertIn('A new list item', response.content.decode())
+        # self.assertTemplateUsed(response, 'home.html')
