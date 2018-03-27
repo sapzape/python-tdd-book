4726825950e8bcad9a63bb1d1d2a803fc667f9e5
PeterKim <31546782+sapzape@users.noreply.github.com>
2018-03-27 22:51:39 +0900

    Redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -64,16 +64,59 @@ class HomePageTest(TestCase):
 
         # response = home_page(request)
 
-        # self.assertIn('신규 작업 아이템', response.content.decode())
-        # expected_html = render_to_string(
-        #     'home.html',
-        #     {'new_item_text': '신규 작업 아이템'}
-        # )
-        # self.assertEqual(response.content.decode(), expected_html)
+        # self.assertEqual(Item.objects.count(), 1)
+        # new_item = Item.objects.first()
+        # self.assertEqual(new_item.text, '신규 작업 아이템')
 
         ## 신규 예제 코드 - Processing a POST Request on the Server
         ## (https://www.obeythetestinggoat.com/book/chapter_post
         ## _and_database.html)
         response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        # request = HttpRequest()
+        # request.method = 'POST'
+        # request.POST['item_text'] = '신규 작업 아이템'
+
+        # response = home_page(request)
+
+        # self.assertEqual(response.status_code, 302)
+        # self.assertEqual(response['location'], '/')
+
+        ## 신규 예제 코드 - Better Unit Testing Practice: Each Test 
+        ##               Should Test One Thing
+        ## (https://www.obeythetestinggoat.com/book/chapter_post
+        ## _and_database.html)
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_only_saves_items_when_necessary(self):
+        # request = HttpRequest()
+        # home_page(request)
+        # self.assertEqual(Item.objects.count(), 0)
+
+        ## 신규 예제 코드 - Saving the POST to the Database
+        ## (https://www.obeythetestinggoat.com/book/
+        ## chapter_post_and_database.html)
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        # request = HttpRequest()
+        # response = home_page(request)
+
+        ## 신규 예제 코드 - Rendering Items in the Template
+        ## (https://www.obeythetestinggoat.com/book/
+        ## chapter_post_and_database.html)
+        response = self.client.get('/')
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
