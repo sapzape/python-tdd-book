1ac58ce1ee74b8e478034a34dfd0b2d1adcf9229
PeterKim <31546782+sapzape@users.noreply.github.com>
2018-03-27 21:56:37 +0900

    Model for list Items and associated migration

--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,28 @@ from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 
+from lists.models import Item
 from lists.views import home_page
+
+
+class ItemModelTest(TestCase):
+    
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = '첫 번째 아이템'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = '두 번째 아이템'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, '첫 번째 아이템')
+        self.assertEqual(second_saved_item.text, '두 번째 아이템')
 
 class HomePageTest(TestCase):
 
@@ -37,22 +58,22 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, 'home.html')
     
     def test_can_save_a_POST_request(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = '신규 작업 아이템'
+        # request = HttpRequest()
+        # request.method = 'POST'
+        # request.POST['item_text'] = '신규 작업 아이템'
 
-        response = home_page(request)
+        # response = home_page(request)
 
-        self.assertIn('신규 작업 아이템', response.content.decode())
-        expected_html = render_to_string(
-            'home.html',
-            {'new_item_text': '신규 작업 아이템'}
-        )
-        self.assertEqual(response.content.decode(), expected_html)
+        # self.assertIn('신규 작업 아이템', response.content.decode())
+        # expected_html = render_to_string(
+        #     'home.html',
+        #     {'new_item_text': '신규 작업 아이템'}
+        # )
+        # self.assertEqual(response.content.decode(), expected_html)
 
         ## 신규 예제 코드 - Processing a POST Request on the Server
         ## (https://www.obeythetestinggoat.com/book/chapter_post
         ## _and_database.html)
-        # response = self.client.post('/', data={'item_text': 'A new list item'})
-        # self.assertIn('A new list item', response.content.decode())
-        # self.assertTemplateUsed(response, 'home.html')
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
