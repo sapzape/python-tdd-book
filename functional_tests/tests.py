5de2b3f3d0a53713f5a47cc8a98cbe3f2efb7807
PeterKim <31546782+sapzape@users.noreply.github.com>
2018-04-10 23:03:27 +0900

    Add a functional test that adjust model so that items are associated with different lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,13 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import time
 
+MAX_WAIT = 10
+
 class NewVisitorTest(LiveServerTestCase):
+
     def setUp(self):
         self.browser = webdriver.Chrome()
         self.browser.implicitly_wait(3)
@@ -11,10 +15,18 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
     
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 에디트(Edith)는 멋진 작업 목록 온라인 앱이 나왔다는 소식을 듣고
@@ -40,19 +52,17 @@ class NewVisitorTest(LiveServerTestCase):
         # 엔터키를 치면 페이지가 갱신되고 작업 목록에
         # "1: 공작깃털 사기" 아이템이 추가된다
         input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: 공작깃털 사기')
+        self. wait_for_row_in_list_table('1: 공작깃털 사기')
 
         # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다
         # 다시 "공작깃털을 이용해서 그물 만들기"라고 입력한다 (에디스는 매우 채계적인 사람이다)
         input_box = self.browser.find_element_by_id('id_new_item')
         input_box.send_keys('공작깃털을 이용해서 그물 만들기')
         input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # 페이지는 다시 갱신되고, 두 개 아이템이 목록에 보인다
-        self.check_for_row_in_list_table('2: 공작깃털을 이용해서 그물 만들기')
-        self.check_for_row_in_list_table('1: 공작깃털 사기')
+        self.wait_for_row_in_list_table('2: 공작깃털을 이용해서 그물 만들기')
+        self.wait_for_row_in_list_table('1: 공작깃털 사기')
 
         # 에디스는 사이트가 입력한 목록을 저장하고 있는지 궁금하다
         # 사이트는 그녀를 위한 특정 URL을 생성해준다
@@ -62,5 +72,46 @@ class NewVisitorTest(LiveServerTestCase):
         # 해당 URL에 접속하면 그녀가 만든 작업 목록이 그대로 있는 것을 확인할 수 있다
 
         # 만족하고 잠자리에 든다
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # Edith start a new to-do list
+        self.browser.get(self.live_server_url)
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('공장깃털 사기')
+        input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: 공장깃털 사기')
+
+        # She notices that her list has a unique URL
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+
+        # 새로운 사용자인 프란시스가 사이트에 접속한다
+
+        ## 새로운 브라우저 세션을 이용해서 에드스의 정보가
+        ## 쿠키를 통해 유입되는 것을 방지한다
+        self.browser.quit()
+        self.browser = webdriver.Chrome()
+
+        # 프란시스가 홈페이지에 접속한다
+        # 에디스의 리스트는 보이지 않는다
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('공작깃털 사기', page_text)
+        self.assertNotIn('그물 만들기', page_text)
+
+        # 프란시스가 새로운 작업 아이템을 입력하기 시작한다
+        # 그는 에디스보다 재미가 없다
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('우유 사기')
+        input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: 우유 사기')
+
+        # 프란시스가 전용 URL을 취득한다
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/list/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # 에디스가 입력한 흔적이 없다는 것을 다시 확인한다
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('공작깃털 사기', page_text)
+        self.assertIn('우유 사기', page_text)
