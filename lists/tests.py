1a13a4824ca795d2abb3d76c506a3d3d3e3e5294
PeterKim <31546782+sapzape@users.noreply.github.com>
2018-05-07 16:15:49 +0900

    Add a URL for creating a new list via POST

--- lists/tests.py
+++ lists/tests.py
@@ -56,55 +56,6 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
-    
-    def test_can_save_a_POST_request(self):
-        # request = HttpRequest()
-        # request.method = 'POST'
-        # request.POST['item_text'] = '신규 작업 아이템'
-
-        # response = home_page(request)
-
-        # self.assertEqual(Item.objects.count(), 1)
-        # new_item = Item.objects.first()
-        # self.assertEqual(new_item.text, '신규 작업 아이템')
-
-        ## 신규 예제 코드 - Processing a POST Request on the Server
-        ## (https://www.obeythetestinggoat.com/book/chapter_post
-        ## _and_database.html)
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
-
-    def test_redirects_after_POST(self):
-        # request = HttpRequest()
-        # request.method = 'POST'
-        # request.POST['item_text'] = '신규 작업 아이템'
-
-        # response = home_page(request)
-
-        # self.assertEqual(response.status_code, 302)
-        # self.assertEqual(response['location'], '/')
-
-        ## 신규 예제 코드 - Better Unit Testing Practice: Each Test 
-        ##               Should Test One Thing
-        ## (https://www.obeythetestinggoat.com/book/chapter_post
-        ## _and_database.html)
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
-
-    def test_only_saves_items_when_necessary(self):
-        # request = HttpRequest()
-        # home_page(request)
-        # self.assertEqual(Item.objects.count(), 0)
-
-        ## 신규 예제 코드 - Saving the POST to the Database
-        ## (https://www.obeythetestinggoat.com/book/
-        ## chapter_post_and_database.html)
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
 
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
@@ -119,3 +70,15 @@ class ListViewTest(TestCase):
 
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
+
+class NewListTest(TestCase):
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
