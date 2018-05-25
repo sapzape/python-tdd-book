15017fa6d72630a4ac10d3aaac5b99b01cbec68a
PeterKim <31546782+sapzape@users.noreply.github.com>
2018-05-25 11:14:43 +0900

    Use Bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,4 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
@@ -6,15 +6,15 @@ import time
 
 MAX_WAIT = 10
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome()
-        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
-    
+
+
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
         while True:
@@ -28,100 +28,99 @@ class NewVisitorTest(LiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
-        # 에디트(Edith)는 멋진 작업 목록 온라인 앱이 나왔다는 소식을 듣고
-        # 해당 웹 사이트를 확인하러 간다
+
+    def test_can_start_a_list_for_one_user(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
         self.browser.get(self.live_server_url)
 
-        # 웹 페이지 타이틀과 헤더가 'To-Do'를 표시하고 있다
+        # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
-        # 그녀는 바로 작업을 추가하기로 한다
-        input_box = self.browser.find_element_by_id('id_new_item')
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
-            input_box.get_attribute('placeholder'),
-            '작업 아이템 입력'
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
         )
 
-        # "공작깃털 사기"라고 텍스트 상자에 입력한다
-        # (에디스의 취미는 날치 잡이용 그물을 만드는 것이다)
-        input_box.send_keys('공작깃털 사기')
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
 
-        # 엔터키를 치면 페이지가 갱신되고 작업 목록에
-        # "1: 공작깃털 사기" 아이템이 추가된다
-        input_box.send_keys(Keys.ENTER)
-        self. wait_for_row_in_list_table('1: 공작깃털 사기')
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list table
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
-        # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다
-        # 다시 "공작깃털을 이용해서 그물 만들기"라고 입력한다 (에디스는 매우 채계적인 사람이다)
-        input_box = self.browser.find_element_by_id('id_new_item')
-        input_box.send_keys('공작깃털을 이용해서 그물 만들기')
-        input_box.send_keys(Keys.ENTER)
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very
+        # methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
-        # 페이지는 다시 갱신되고, 두 개 아이템이 목록에 보인다
-        self.wait_for_row_in_list_table('2: 공작깃털을 이용해서 그물 만들기')
-        self.wait_for_row_in_list_table('1: 공작깃털 사기')
+        # The page updates again, and now shows both items on her list
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
-        # 에디스는 사이트가 입력한 목록을 저장하고 있는지 궁금하다
-        # 사이트는 그녀를 위한 특정 URL을 생성해준다
-        # 이때 URL에 대한 설명도 함께 제공된다
-        # self.fail('Finish the test!')
+        # Satisfied, she goes back to sleep
 
-        # 해당 URL에 접속하면 그녀가 만든 작업 목록이 그대로 있는 것을 확인할 수 있다
-
-        # 만족하고 잠자리에 든다
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
-        # Edith start a new to-do list
+        # Edith starts a new to-do list
         self.browser.get(self.live_server_url)
-        input_box = self.browser.find_element_by_id('id_new_item')
-        input_box.send_keys('공작깃털 사기')
-        input_box.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: 공작깃털 사기')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         # She notices that her list has a unique URL
         edith_list_url = self.browser.current_url
         self.assertRegex(edith_list_url, '/lists/.+')
 
-        # 새로운 사용자인 프란시스가 사이트에 접속한다
+        # Now a new user, Francis, comes along to the site.
 
-        ## 새로운 브라우저 세션을 이용해서 에드스의 정보가
-        ## 쿠키를 통해 유입되는 것을 방지한다
+        ## We use a new browser session to make sure that no information
+        ## of Edith's is coming through from cookies etc
         self.browser.quit()
         self.browser = webdriver.Chrome()
 
-        # 프란시스가 홈페이지에 접속한다
-        # 에디스의 리스트는 보이지 않는다
+        # Francis visits the home page.  There is no sign of Edith's
+        # list
         self.browser.get(self.live_server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
-        self.assertNotIn('공작깃털 사기', page_text)
-        self.assertNotIn('그물 만들기', page_text)
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
 
-        # 프란시스가 새로운 작업 아이템을 입력하기 시작한다
-        # 그는 에디스보다 재미가 없다
-        input_box = self.browser.find_element_by_id('id_new_item')
-        input_box.send_keys('우유 사기')
-        input_box.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: 우유 사기')
+        # Francis starts a new list by entering a new item. He
+        # is less interesting than Edith...
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
 
-        # 프란시스가 전용 URL을 취득한다
+        # Francis gets his own unique URL
         francis_list_url = self.browser.current_url
         self.assertRegex(francis_list_url, '/lists/.+')
         self.assertNotEqual(francis_list_url, edith_list_url)
 
-        # 에디스가 입력한 흔적이 없다는 것을 다시 확인한다
+        # Again, there is no trace of Edith's list
         page_text = self.browser.find_element_by_tag_name('body').text
-        self.assertNotIn('공작깃털 사기', page_text)
-        self.assertIn('우유 사기', page_text)
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # Satisfied, they both go back to sleep
+
 
     def test_layout_and_styling(self):
         # Edith goes to the home page
         self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024, 768)
 
-        # She notice the input box is nicely centered
+        # She notices the input box is nicely centered
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
@@ -130,7 +129,7 @@ class NewVisitorTest(LiveServerTestCase):
         )
 
         # She starts a new list and sees the input is nicely
-        # centerd there too
+        # centered there too
         inputbox.send_keys('testing')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: testing')
