0f658768d6b9ca081840fb1bd5a66d63c7c3e20e
PeterKim <31546782+sapzape@users.noreply.github.com>
2018-06-13 10:35:01 +0900

    Update fabfile to add  automatically the EMAIL_PASSWORD to the .env file on the server

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -1,6 +1,8 @@
 import random
 from fabric.contrib.files import append, exists
 from fabric.api import cd, env, local, run
+import os
+
 
 REPO_URL = 'https://github.com/sapzape/python-tdd-book.git'
 
@@ -36,6 +38,8 @@ def _create_or_update_dotenv():
             'abcdefghijklmnopqrstuvwxyz0123456789', k=50
         ))
         append('.env', f'DJANGO_SECRET_KEY={new_secret}')
+    email_password = os.environ['EMAIL_PASSWORD']
+    append('.env', f'EMAIL_PASSWORD={email_password}')
 
 def _update_static_files():
     run('./virtualenv/bin/python manage.py collectstatic --noinput')
